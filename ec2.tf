9d3165b298137a0d601874770a5c35bf587ae009
rajesh veeramallu <rajesh45v@mail.com>
2025-04-19 00:14:29 +0530

    demo data

--- ec2.tf
+++ ec2.tf
@@ -1,27 +1,24 @@
 resource "aws_security_group" "allow_ssh" {
-    name        = "allow_ssh"
-    description = "allowing SSH accesss"
-    
+    name = "allow_ssh"
+    description = "allowing SSH access"
 
-    ingress { 
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
+    #terraform block
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-
-     tags = {
-          Name = "allow_ssh"
-          created by = "rajesh"
-  }
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "rajesh"
+    }
 }
